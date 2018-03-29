050f940ca9dccb961bc900a5e100d8484b2bc51d
Gus Henry Smith <gus@justg.us>
2018-03-28 23:35:20 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# Organization of this file is based on:
+# https://github.com/ckmarkoh/GAN-tensorflow/blob/master/gan.py
+# Usage of GRUCell/building of an RNN with help from:
+# https://danijar.com/introduction-to-recurrent-networks-in-tensorflow/
+
+import tensorflow as tf
+
+def build_generator(z_prior):
+  
+  num_layers = 3
+  num_units = 200
+  
+  cells = []
+  for _ in range(num_layers):
+    cell = tf.contrib.rnn.GRUCell()
+    # TODO dropout?
+    cells.append(cell)
