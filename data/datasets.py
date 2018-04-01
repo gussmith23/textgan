caca0842e4eccffff49cb02fa1f6ea0b77349aeb
Gus Henry Smith <gus@justg.us>
2018-04-01 16:15:25 -0400

    adds file for getting datasets

---
+++ data/datasets.py
@@ -0,0 +1,7 @@
+def get(name):
+  if name == 'babblebuds':
+    from data.babblebuds.babblebuds import get_data()
+    return get_data()
+  
+  else: 
+    raise ValueError("Unrecognized dataset {}.".format(name))
