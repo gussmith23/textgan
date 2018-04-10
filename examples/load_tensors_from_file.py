ae92f9615e51662048fbc9a49b353db8eacf5892
Gus Henry Smith <gus@justg.us>
2018-04-10 18:47:21 -0400

    adds example

---
+++ examples/load_tensors_from_file.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+
+
+embeddings = tf.Variable(-1.0, validate_shape=False, name="normalized_embeddings")
+saver = tf.train.Saver()
+
+with tf.Session() as sess:
+  saver.restore(sess, "log/embeddings_model.ckpt")
+  out_embeddings = sess.run([embeddings])
+  
+print(out_embeddings)
+
+graph = tf.Graph()
+with graph.as_default(): 
+  embeddings = tf.Variable(-1.0, validate_shape=False, name="normalized_embeddings")
+  init = tf.global_variables_initializer()
+  saver = tf.train.Saver()
+
+with tf.Session(graph=graph) as sess:
+  init.run()
+  print(sess.run([embeddings]))
