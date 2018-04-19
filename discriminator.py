bf7c5c62b3a12c1fc5afcacd6e4745c6fb61f05c
Gus Henry Smith <gus@justg.us>
2018-04-18 21:46:03 -0400

    updates discriminator model

--- discriminator.py
+++ discriminator.py
@@ -1,40 +1,30 @@
-def build_discriminator(x_data, x_generated):
+import tensorflow as tf
+
+# TODO it would be nice to not have to pass the sizes in automatically, but
+# I can't figure it out.
+def build_discriminator(x_data, x_generated, batch_size, sentence_length, embedding_size):
   """
   assuming that these come in as shape [batch_size, sentence_length, embedding_size]
   all sentences must be padded to the same length.
   """
-  # TODO my asserts are all messed up.
-  # assert(x_data.dtype == x_generated.dtype)
-  # assert(x_data.shape.as_list() == x_generated.shape.as_list())
-  # assert(x_data.shape[2] == x_generated.shape[2])
-  
-  # sentence_length = tf.maximum(tf.shape(x_data)[0], tf.shape(x_generated)[0])
-  
-  # first, make sure that they're the same size.
-  # this is is kind of a hack that relies on broadcasting, but it's simple.
-  # https://stackoverflow.com/questions/34362193/how-to-explicitly-broadcast-a-tensor-to-match-anothers-shape-in-tensorflow?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
-  # x_data = tf.pad(x_data, [[0,0], [0, max_sentence_length-tf.shape(x_data)[0]], [0,0]], 'CONSTANT', constant_values=0)
-  # x_generated = tf.pad(x_generated, [[0,0], [0, max_sentence_length-tf.shape(x_generated)[0]], [0,0]], 'CONSTANT', constant_values=0)
-  # x_data.set_shape([batch_size,max_sentence_length,embedding_size])
-  # x_generated.set_shape([batch_size,max_sentence_length,embedding_size])
     
   # concatenate batches
   x_in = tf.concat([x_data, x_generated], 0) 
   x_in = tf.expand_dims(x_in,3) # add channel dimension
-  assert(x_in.get_shape().as_list() == [2*batch_size, max_sentence_length, embedding_size, 1])
+  assert(x_in.get_shape().as_list() == [2*batch_size, sentence_length, embedding_size, 1])
   
   # building the CNN with help from
   # - Kim 2014 (which describes the CNN)
   # - https://github.com/tensorflow/models/blob/master/tutorials/image/cifar10/cifar10.py
   
-  with tf.variable_scope('conv1') as scope:
+  with tf.variable_scope('conv') as scope:
     # filter: [filter_height, filter_width, in_channels, out_channels]
     # height is the number of words we want, while width should be the size of the embedding (always).
     # from Kim 2014: filter windows (h) of 3, 4, 5 with 100 feature maps each
     # TODO how do i handle 3, 4, and 5 simultaneously? can i?
     num_words = 3; num_filters = 100
-    conv1_filter = tf.Variable(tf.random_normal([num_words, embedding_size, 1, num_filters]))
-    conv1_bias = tf.Variable(tf.random_normal([num_filters])) # TODO initialize to zero?
+    conv1_filter = tf.Variable(tf.random_normal([num_words, embedding_size, 1, num_filters]), name="weights")
+    conv1_bias = tf.Variable(tf.random_normal([num_filters]), name="bias") # TODO initialize to zero?
     conv = tf.nn.conv2d(x_in, conv1_filter, [1, 1, 1, 1], padding='VALID')
     conv += conv1_bias 
     
@@ -52,13 +42,13 @@ def build_discriminator(x_data, x_generated):
   
   # TODO no dropout implemented yet
   
-  with tf.variable_scope('fullyconnected') as scope:
+  with tf.variable_scope('fc') as scope:
     # Move everything into depth so we can perform a single matrix multiply.
     reshape = tf.reshape(pool1, [x_in.get_shape().as_list()[0], -1])
     # TODO can this be computed statically? i think it can, i'm just too lazy to do it right now.
     dim = tf.shape(reshape)[1]
-    fc_weights = tf.Variable(tf.random_normal([dim, 2]), validate_shape=False)
-    fc_bias = tf.Variable(tf.random_normal([2])) # TODO initialize to zero?
+    fc_weights = tf.Variable(tf.random_normal([dim, 2]), validate_shape=False, name="weights")
+    fc_bias = tf.Variable(tf.random_normal([2]), name="bias") # TODO initialize to zero?
     fc = tf.matmul(reshape, fc_weights) + fc_bias
     fc = tf.identity(fc, name=scope.name)
 
