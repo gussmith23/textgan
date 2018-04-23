7553a99a715fb72300f04a84454f8e76389cb614
Gus Henry Smith <gus@justg.us>
2018-04-23 10:24:16 -0400

    removes visualization code and uses numpy to save

--- embeddings-old.py
+++ embeddings-old.py
@@ -143,36 +143,4 @@ with tf.Session(graph=graph) as session:
             _, cur_loss = session.run([optimizer, loss], feed_dict=feed_dict)
         saver.save(session, os.path.join(FLAGS.log_dir,
                                          'embeddings_model.ckpt'))
-
-    final_embeddings = normalized_embeddings.eval()
-    import pickle
-    pickle.dump(final_embeddings, open("embeddings.p", "wb"))
-
-
-# Visualization
-def plot_with_labels(low_dim_embs, labels, filename):
-    assert low_dim_embs.shape[0] >= len(labels), 'More labels than embeddings'
-    plt.figure(figsize=(18, 18))  # in inches
-    for i, label in enumerate(labels):
-        x, y = low_dim_embs[i, :]
-        plt.scatter(x, y)
-        plt.annotate(
-            label,
-            xy=(x, y),
-            xytext=(5, 2),
-            textcoords='offset points',
-            ha='right',
-            va='bottom')
-
-    plt.savefig(filename)
-
-
-from sklearn.manifold import TSNE
-import matplotlib.pyplot as plt
-
-tsne = TSNE(
-    perplexity=30, n_components=2, init='pca', n_iter=5000, method='exact')
-plot_only = 500
-low_dim_embs = tsne.fit_transform(final_embeddings[:plot_only, :])
-labels = [reversed_dictionary[i] for i in range(plot_only)]
-plot_with_labels(low_dim_embs, labels, 'tsne.png')
+        np.save("embeddings-old.npy", normalized_embeddings.eval())
