34411f09671ff697fea397199320bead9a8e59a8
Gus Henry Smith <gus@justg.us>
2018-04-24 14:11:14 -0400

    adds docstring and changes formatting

--- data/datasets.py
+++ data/datasets.py
@@ -1,7 +1,9 @@
 def get(name):
-  if name == 'babblebuds':
-    from data.babblebuds.babblebuds import get_data
-    return get_data()
-  
-  else: 
-    raise ValueError("Unrecognized dataset {}.".format(name))
+    """Get raw dataset (not split into train/test/validation.)
+    """
+    if name == 'babblebuds':
+        from data.babblebuds.babblebuds import get_data
+        return get_data()
+
+    else:
+        raise ValueError("Unrecognized dataset {}.".format(name))
